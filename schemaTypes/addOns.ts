25024408779958805bf801fbe27e477489428f54
mysticalwardrobes <mysticalwardrobes01@gmail.com>
2026-09-15 22:27:18 +0800

    fix repp

--- schemaTypes/addOns.ts
+++ schemaTypes/addOns.ts
@@ -1,4 +1,4 @@
-import {defineArrayMember, defineField, defineType} from 'sanity'
+import { defineArrayMember, defineField, defineType } from 'sanity'
 
 const addOnTypes = [
   'hood',
@@ -23,30 +23,30 @@ export const addOnsType = defineType({
   title: 'Add Ons',
   type: 'document',
   fields: [
-    defineField({name: 'name', title: 'Name', type: 'string', validation: (rule) => rule.required()}),
+    defineField({ name: 'name', title: 'Name', type: 'string', validation: (rule) => rule.required() }),
     defineField({
       name: 'description',
       title: 'Description',
       type: 'array',
-      of: [defineArrayMember({type: 'block'}), defineArrayMember({type: 'image'})],
+      of: [defineArrayMember({ type: 'block' }), defineArrayMember({ type: 'image' })],
     }),
     defineField({
       name: 'type',
       title: 'Type',
       type: 'string',
-      options: {list: addOnTypes, layout: 'dropdown'},
-      validation: (rule) => rule.required().valid(...addOnTypes.map(({value}) => value)),
+      options: { list: addOnTypes, layout: 'dropdown' },
+      validation: (rule) => rule.required().valid(...addOnTypes.map(({ value }) => value)),
     }),
-    defineField({name: 'metroManilaRate', title: 'Metro Manila Rate', type: 'number'}),
-    defineField({name: 'luzonRate', title: 'Luzon Rate', type: 'number'}),
-    defineField({name: 'outsideLuzonRate', title: 'Outside Luzon Rate', type: 'number'}),
-    defineField({name: 'forSaleRate', title: 'For Sale Rate', type: 'number'}),
+    defineField({ name: 'metroManilaRate', title: 'Metro Manila Rate', type: 'number' }),
+    defineField({ name: 'luzonRate', title: 'Luzon Rate', type: 'number' }),
+    defineField({ name: 'outsideLuzonRate', title: 'Outside Luzon Rate', type: 'number' }),
+    defineField({ name: 'forSaleRate', title: 'For Sale Rate', type: 'number' }),
     defineField({
       name: 'pictures',
       title: 'Pictures',
       type: 'array',
-      of: [defineArrayMember({type: 'image'})],
+      of: [defineArrayMember({ type: 'image' })],
     }),
-    defineField({name: 'contentfulArchived', title: 'Contentful Archived', type: 'boolean', readOnly: true}),
+    defineField({ name: 'contentfulArchived', title: 'Contentful Archived', type: 'boolean', readOnly: true }),
   ],
 })
